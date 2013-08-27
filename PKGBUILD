5418ad83448786e6da40d818e80fa9f8c49eff6c
Mark K Cowan <mark@battlesnake.co.uk>
2013-08-27 15:59:58 +0100

    Added comments, removed boot2

--- PKGBUILD
+++ PKGBUILD
@@ -1,40 +1,33 @@
-# See http://wiki.archlinux.org/index.php/VCS_PKGBUILD_Guidelines
-# for more information on packaging from GIT sources.
-
 # Maintainer: Mark K Cowan <mark@battlesnake.co.uk>
 pkgname=kernel-fglrx-custom-git
 pkgver=0.1
 pkgrel=1
-pkgdesc="Script to build a recent kernel (tested on 3.4) with patch for fglrx.  Created as I have a Radeon HD4000 series, which AMD have dropped Linux support for."
-arch=('x86_64')
+pkgdesc="Script to build a recent kernel (tested on 3.4) with patch for fglrx.	Created as I have a Radeon HD4000 series, which AMD have dropped Linux support for."
+arch=( 'x86_64' )
 url="https://github.com/battlesnake/kernel-fglrx-custom"
-license=('GPL2')
+license=( 'GPL2' )
 depends=( 'make' 'catalyst-hook' 'mkinitcpio' 'grub' )
-makedepends=('git')
+makedepends=( 'git' )
 provides=( 'kernel-fglrx-custom' )
-source=('git://github.com/battlesnake/kernel-fglrx-custom.git')
-md5sums=('SKIP')
+source=( 'git://github.com/battlesnake/kernel-fglrx-custom.git' )
+md5sums=( 'SKIP' )
 
 _gitroot=github.com/battlesnake/kernel-fglrx-custom
 _gitname=kernel-fglrx-custom
 
-build() {
-  cd "$srcdir"
-  msg "Connecting to GIT server...."
-
-  if [[ -d "$_gitname" ]]; then
-    cd "$_gitname" && git pull origin
-    msg "The local files are updated."
-  else
-    git clone "$_gitroot" "$_gitname"
-  fi
-
-  msg "GIT checkout done or server timeout"
-}
-
 package() {
-  msg "Copying script to $pkgdir/"
-  cd "$srcdir/$_gitname/*.sh" "$pkgdir/"
+	cd "$srcdir"
+		
+	msg "Getting script from github...."
+	
+	if [[ -d "$_gitname" ]]
+	then	cd "$_gitname" && git pull origin
+	else	git clone "$_gitroot" "$_gitname"
+	fi
+	
+	msg "GIT checkout done or server timeout"
+	
+	msg "Copying script to $pkgdir/"
+	
+	cd "$srcdir/$_gitname/*.sh" "$pkgdir/"
 }
-
-# vim:set ts=2 sw=2 et:
--- kernel-fglrx-custom.sh
+++ kernel-fglrx-custom.sh
@@ -1,44 +1,77 @@
 #!/bin/bash
 
+# kernel-fglrx-custom.sh
+#
+# Kernel build script to enable use of old Radeons on newer (but not
+# quite _really new_) kernels.
+#
+# Mark K Cowan, mark@battlesnake.co.uk
+#
+# https://github.com/battlesnake/kernel-fglrx-custom
+
 # The kernel version number
 VERSION=3.4.59
 # The kernel flavour (e.g. lts, arch, lowlatency, etc)
 FLAVOUR=fglrx-custom
 
+# For now, run as root.  I trust the kernel developers to produce safe,
+# virus-free, bug-free makefiles.  They are a clever bunch of people
+# after all.  Since building as root is a cardinal sin to some, someone
+# else can modify this to do the appropriate chrooting and sudo stuff,
+# if they so desire.
+#
+# Untested features:
+#  + Kernel version != 3.4.59
+#  + Filenames/paths containing spaces
+#  + Auto-downloading kernel
+#  + Auto-moving script to download folder
+#
 # The kernel source should be extracted to:
 #   /usr/src/linux-$VERSION-$FLAVOUR
+# Alternatively, be a nice guinea pig and test the automatic download by
+# running this script from any folder you can write to, after setting the
+# VERSION variable to the kernel that you want to auto-download.
 #
-# Copy this script to that folder, make it your working directory then execute
-# this script.  Alternatively, modify the SRCDIR variable.
+# If you aren't a guinea pig, copy this script to the kernel source folder,
+# make it your working directory then execute this script.  Alternatively,
+# modify the SRCDIR variable.
 #
-# Hence the VERSION and FLAVOUR can actually be anything you want, as long as
-# the above path is valid and points to the kernel source.
+# The VERSION and FLAVOUR can actually be anything you want, as long as
+# the above path is valid and points to the kernel source.  Otherwise, the
+# VERSION must be a valid kernel.org version number, for the untested automatic
+# download to be able to find a source.
 #
 # The output of the build is located in:
+#  $SRCDIR/$OUTDIR if SRCDIR has been modified.
+# which by default would be:
 #   /usr/src/linux-$VERSION-$FLAVOUR/$OUTDIR
+# if you extracted a kernel source to that folder, set it as the working
+# directory and ran the script from it.
 #
-# I have three operating systems (Arch / Ubuntu / Windows 2003) across two SSDs
-# so I stuck a grub boot partition on each SSD to provide me with:
+# Note regarding the purpose of the commented "boot" lines at the end:
+#   I have three operating systems (Arch / Ubuntu / Windows 2003) across two
+# SSDs so I stuck a grub boot partition on each SSD to provide me with:
 #  a) a backup bootloader should an Ubuntu update (predictably) screw one up
 #  b) the ability to pull a disk and boot it straight away in another PC easily
-#
 # The boot partitions are mounted in /boot and /boot2, so this script will
 # put a vmlinuz*, initrd*.img, and grub/grub.cfg on on BOTH boot partitions.
 # Other people may want to remove the lines containing "/boot2/" near the end
-# of this script.
+# of this script for clarity.
 #
 # The script MAY be able to download the kernel source and prepare it, but this
-# has NOT been tested at all and in all probability probably won't work.
+# has NOT been tested at all.
 
+# URL of kernel source - not used if a source is found in $PWD/
 KERNELSOURCEURL="https://www.kernel.org/pub/linux/kernel/v3.0/linux-$VERSION.tar.xz"
 
 # The source directory (default: current working directory)
 SRCDIR="$PWD"
 
-# The subdirectory that the build should output to
-OUTDIR="_build"
+# The subdirectory of SRCDIR that the build should output to
+OUTDIR="build"
 
 # The maximum number of concurrent jobs MAKE can run
+# Set to 16, the kernel builds in <2minutes on my 5.1GHz i7-2700k
 JOBS=16
 
 # Force (-f / --force) option skips all prompts
@@ -81,11 +114,13 @@ function failed {
 }
 
 # Download a kernel source if we don't have one in the current directory
+# Symlink it to the recommended target directory
+# Copy this script to that directory
 if [ ! -e "Kbuild" ]
 then	start "download kernel source"
 	curl "$KERNELSOURCEURL" | tar xJ || failed
-	ln -s "$PWD/linux-$VERSION" "/usr/src/linux-$VERSION-$FLAVOUR" || failed
 	SRCDIR="/usr/src/linux-$VERSION-$FLAVOUR"
+	ln -s "$PWD/linux-$VERSION" "$SRCDIR" || failed
 	cp "$0" "$SRCDIR/"
 fi
 
@@ -135,27 +170,32 @@ make modules_install O="$OUTDIR" --jobs=$JOBS || failed
 
 # AMD Radeon patch (required for catalyst build script, called by catalyst-hook, called by mkinitcpio)
 start "apply radeon patch"
-echo "#define COMPAT_ALLOC_USER_SPACE arch_compat_alloc_user_space" >> "$SRCDIR/_build/arch/x86/include/generated/asm/compat.h"
-if [ -e "$SRCDIR/_build/arch/x86/include/asm" ]
-then	rm "$SRCDIR/_build/arch/x86/include/asm" || failed
+echo "#define COMPAT_ALLOC_USER_SPACE arch_compat_alloc_user_space" >> "$SRCDIR/$OUTDIR/arch/x86/include/generated/asm/compat.h"
+if [ -e "$SRCDIR/$OUTDIR/arch/x86/include/asm" ]
+then	rm "$SRCDIR/$OUTDIR/arch/x86/include/asm" || failed
 fi
-ln -s "$SRCDIR/_build/arch/x86/include/generated/asm" "$SRCDIR/_build/arch/x86/include/asm" || failed
+ln -s "$SRCDIR/$OUTDIR/arch/x86/include/generated/asm" "$SRCDIR/$OUTDIR/arch/x86/include/asm" || failed
 
-# Creating initramfs
+# Creating initramfs (don't compress, we're loading off an SSD)
 start "create initramfs"
 mkinitcpio -g "$OUTDIR/initramfs.img" -z cat -k $VERSION-$FLAVOUR || failed
 
 # Installing kernel
 start "install kernel"
 cp "$OUTDIR/arch/x86/boot/bzImage" /boot/vmlinuz-$FLAVOUR || failed
-cp "$OUTDIR/arch/x86/boot/bzImage" /boot2/vmlinuz-$FLAVOUR
+#cp "$OUTDIR/arch/x86/boot/bzImage" /boot2/vmlinuz-$FLAVOUR
 
 # Installing initrd
 start "install initramfs"
 cp "$OUTDIR/initramfs.img" /boot/initramfs-$FLAVOUR.img || failed
-cp "$OUTDIR/initramfs.img" /boot2/initramfs-$FLAVOUR.img
+#cp "$OUTDIR/initramfs.img" /boot2/initramfs-$FLAVOUR.img
 
 # Updating grub
 start "generate grub.cfg"
 grub-mkconfig -o /boot/grub/grub.cfg || failed
-grub-mkconfig -o /boot2/grub/grub.cfg
+#grub-mkconfig -o /boot2/grub/grub.cfg
+
+# Reboot and we have working AMD drivers on an old Radon with a new kernel!
+echo "If all went well, then please reboot and choose the custom kernel in GRUB for"
+echo "the update to take effect.  If the new kernel refuses to boot, then reboot,"
+echo "choose your old kernel, then have a fun debugging session."
